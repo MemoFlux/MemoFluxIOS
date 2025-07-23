019d0f0e79ce0c5514d6c5b86902113dbb5ef4fd
Raspberry... <mashuo2855190178@icloud.com>
2025-07-24 01:15:52 +0800

    feat: xcproject init

---
+++ MemoFlux/MemoFlux/MemoFluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoFluxApp.swift
+//  MemoFlux
+//
+//  Created by 马硕 on 2025/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoFluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
