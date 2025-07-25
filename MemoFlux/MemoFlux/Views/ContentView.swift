726fbfba96f8f7f48d264e51d2fcfa9ab75133f7
Raspberry... <mashuo2855190178@icloud.com>
2025-07-25 16:51:38 +0800

    feat: optimize tabBar behaviour

--- MemoFlux/MemoFlux/Views/ContentView.swift
+++ MemoFlux/MemoFlux/Views/ContentView.swift
@@ -9,6 +9,23 @@ import SwiftUI
 import UIKit
 
 struct ContentView: View {
+
+  init() {
+    // TabBar 外观设置
+    let tabBarAppearance = UITabBarAppearance()
+    tabBarAppearance.configureWithDefaultBackground()
+    tabBarAppearance.backgroundColor = UIColor.globalStyleBackgroundColor
+    tabBarAppearance.shadowColor = UIColor.clear
+
+    let scrollEdgeAppearance = UITabBarAppearance()
+    scrollEdgeAppearance.configureWithOpaqueBackground()
+    scrollEdgeAppearance.backgroundColor = UIColor.globalStyleBackgroundColor
+    scrollEdgeAppearance.shadowColor = UIColor.clear
+
+    UITabBar.appearance().standardAppearance = tabBarAppearance
+    UITabBar.appearance().scrollEdgeAppearance = scrollEdgeAppearance
+  }
+
   var body: some View {
     TabView {
       HomePageView()
